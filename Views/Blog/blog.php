7e9eb5f5fc8e8480f02c9c43283aa7746fd30644
MrNavySeals <dsparrado197023@gmail.com>
2025-05-11 22:23:58 -0500

    dev

--- Views/Blog/blog.php
+++ Views/Blog/blog.php
@@ -111,6 +111,7 @@
                             <li>2</li>
                             <li>3</li>
                             <li>4</li>
+                            <li>5</li>
                             <li><i class="fas fa-angle-right"></i></li>
                         </ul>
                     </div>
