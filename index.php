88d817a322688e522b2c371a3f99114eba09f928
MrNavySeals <dsparrado197023@gmail.com>
2025-06-08 21:23:21 -0500

    dev

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
     $url = !empty($_GET['url']) ? $_GET['url'] : 'home/home';
     require_once ("Config/Config.php");
-    require_once ("Libraries/core/Errores.php");
+    require_once ("Libraries/Core/Errores.php");
     require_once ("Helpers/Helpers.php");
     require_once("Libraries/Core/Autoload.php");
-    require_once ("Libraries/core/Router.php");
+    require_once ("Libraries/Core/Router.php");
     require_once ("Libraries/TCPDF/tcpdf.php");
     require_once ("Libraries/vendor/autoload.php");
     require_once ("Libraries/html2pdf/vendor/autoload.php");
